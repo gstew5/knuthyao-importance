ad552904043e9856c3f454956c936129c22d963d
Gordon Stewart <gstew5@gmail.com>
2019-05-10 14:57:17 -0400

    A bit more work

--- ky-backend/src/main.rs
+++ ky-backend/src/main.rs
@@ -1,24 +1,67 @@
-use hashbrown::hash_map::HashMap;
+extern crate rand;
+use rand::prelude::*;
+use std::time::{Instant};
 
-mod lut;
-use self::lut::*;
-
-#[derive(Debug,Clone)]
+#[derive(Debug,Clone,Copy,PartialEq)]
 enum Event {
     A,
     B,
-    C
+}
+
+enum Tree<A> {
+    Leaf(A),
+    Node(Box<Tree<A>>, Box<Tree<A>>),
+    Hole
+}
+
+use Tree::*;
+
+type Index = u16;
+const NUM_BITS: usize = 16;
+const NUM_ENTRIES: usize = 1<<NUM_BITS;
+
+fn event_of<A: Clone>(t: &Tree<A>, i: Index) -> Option<A> {
+    match t {
+        Leaf(a) => Some(a.clone()),
+        Node(t1, t2) =>
+            if i % 2 == 0 {
+                event_of(&*t1, i>>1)
+            } else {
+                event_of(&*t2, i>>1)
+            },
+        Hole => None
+    }
+}
+
+fn materialize<A: Clone>(t: &Tree<A>, entries: &mut [Option<A>]) -> () {
+    for i in 0..NUM_ENTRIES {
+        entries[i] = event_of(t, i as u16)
+    }
 }
 
 fn main() {
-    let mut lut: HashMap<u8, (u8, Event)> = PLUT::new();
+    let tree: Tree<Event> =
+        Node(Box::new(Leaf(Event::A)),
+             Box::new(Node(Box::new(Leaf(Event::B)),
+                           Box::new(Hole))));
+    let mut entries: [Option<Event>; NUM_ENTRIES] = [None; NUM_ENTRIES];
+    materialize(&tree, &mut entries[..]);
 
-    lut.ins(3, 8, Event::A).expect("error");
-    lut.ins(1<<7, 2, Event::B).expect("error");
-    lut.ins(3<<6, 2, Event::C).expect("error");    
-
-    println!("lut = {:?}", lut);
-    
-    let v = lut.par_get(3<<6 | 1).expect("error");
-    println!("v = {:?}", v);
+    let mut num_as = 0;
+    let mut num_bs = 0;
+    let mut num_nones = 0;
+    let mut rng = thread_rng();
+    let now = Instant::now();    
+    for _ in 0..10_000_000 {
+        let i: Index = rng.gen();
+        match entries[i as usize] {
+            Some(Event::A) => num_as += 1,
+            Some(Event::B) => num_bs += 1,
+            None => num_nones += 1,
+        }
+    };
+    let elapsed = now.elapsed();    
+    println!("As = {}, Bs = {}, Nones = {}", num_as, num_bs, num_nones);
+    println!("time = {}s, {}ms", elapsed.as_secs(), elapsed.subsec_millis())    
 }
+
