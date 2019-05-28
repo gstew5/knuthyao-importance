0ce02e8723d5351fe451d94bafe1c6905a66800e
Gordon Stewart <gstew5@gmail.com>
2019-05-28 14:23:30 -0400

    Unfold trees

--- ky-backend/src/main.rs
+++ ky-backend/src/main.rs
@@ -33,6 +33,19 @@ fn event_of<A: Clone>(root: &Tree<A>, t: &Tree<A>, i: Index) -> Option<A> {
     }
 }
 
+fn event_of<A: Clone>(t_orig: &Tree<A>, t: &Tree<A>, i: Index) -> Option<A> {
+    match t {
+        Leaf(a) => Some(a.clone()),
+        Node(t1, t2) =>
+            if i % 2 == 0 {
+                event_of(t_orig, &*t1, i>>1)
+            } else {
+                event_of(t_orig, &*t2, i>>1)
+            },
+        Hole => event_of(t_orig, t_orig, i)
+    }
+}
+
 fn materialize<A: Clone>(t: &Tree<A>, entries: &mut [Option<A>]) -> () {
     for i in 0..NUM_ENTRIES {
         entries[i] = event_of(t, t, i as u16)
@@ -50,9 +63,25 @@ fn main() {
     let mut num_as = 0;
     let mut num_bs = 0;
     let mut num_nones = 0;
+    
+    let now0 = Instant::now();
+    for _ in 0..NUM_TRIALS {
+        match rand_event(&tree) {
+            Some(Event::A) => num_as += 1,
+            Some(Event::B) => num_bs += 1,
+            None => num_nones += 1,
+        }
+    }
+    let elapsed0 = now0.elapsed();
+    println!("As = {}, Bs = {}, Nones = {}", num_as, num_bs, num_nones);
+    println!("time = {}s, {}ms", elapsed0.as_secs(), elapsed0.subsec_millis());
+
+    num_as = 0;
+    num_bs = 0;
+    num_nones = 0;
     let mut rng = thread_rng();
     let now = Instant::now();    
-    for _ in 0..10_000_000 {
+    for _ in 0..NUM_TRIALS {
         let i: Index = rng.gen();
         match entries[i as usize] {
             Some(Event::A) => num_as += 1,
